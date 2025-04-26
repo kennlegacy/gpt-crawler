c73b2655908b377fbfce12c3807855522ec0800d
kennlegacy <info.kennishaprince@gmail.com>
2025-04-26 18:41:31 -0400

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  url: "https://bellatorlife.com/",
+  match: "https://bellatorlife.com/",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
   maxTokens: 2000000,
